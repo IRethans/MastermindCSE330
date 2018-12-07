44d6f05e1c039bbc839e345e1cc167eaddae3a51
Isa Rethans <isarethans@hotmail.com>
2018-12-07 16:02:18 +0100

    k

--- gameapp/public/javascripts/game.js
+++ gameapp/public/javascripts/game.js
@@ -1,3 +1,8 @@
+var currentline = 1;
+
 $(".cb img").on("click", function (event) {
-    alert($(this).attr('id'));
+    var currentid = $(this).attr('id');
+    document.getElementById(currentid).setAttribute("src", "images/rood.png");
 });
+
+
