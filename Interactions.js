0859cfc3903ed0116b928ae321340c87a740e101
Pien Kastelein <pkastelein@live.nl>
2018-12-07 16:40:08 +0100

    jdo

--- Interactions.js
+++ Interactions.js
@@ -1,7 +1,32 @@
-function chooseBalls(){
-    Array.from(balls).forEach(function(el)){
-        el.addEventListener('click', function singleClick(e)){
-            
-        }
-    }
-}
+var currentline = 1;
+
+$(".cb img").on("click", function (event) {
+    currentid = $(this).attr('id');
+    document.getElementById('line6rA').innerHTML = "<img src = 'images/rood.png'>";
+});
+
+
+
+$(".A").on("click", function (event) {
+    currentid = $(this).attr('id');
+    document.getElementById(currentid).innerHTML = "<img src = 'images/game-ball.png'>";
+});
+$(".B").on("click", function (event) {
+    currentid = $(this).attr('id');
+    document.getElementById(currentid).innerHTML = "<img src = 'images/game-ball.png'>";
+});
+$(".C").on("click", function (event) {
+    currentid = $(this).attr('id');
+    document.getElementById(currentid).innerHTML = "<img src = 'images/game-ball.png'>";
+});
+$(".D").on("click", function (event) {
+    currentid = $(this).attr('id');
+    document.getElementById(currentid).innerHTML = "<img src = 'images/game-ball.png'>";
+});
+
+
+
+$(".cb img").on("click", function (event) {
+    currentid = $(this).attr('id');
+    document.getElementById('line1lD').innerHTML = "<img src = 'images/rood.png'>";
+});
--- gameapp/public/javascripts/game.js
+++ gameapp/public/javascripts/game.js
@@ -2,7 +2,35 @@ var currentline = 1;
 
 $(".cb img").on("click", function (event) {
     currentid = $(this).attr('id');
-    document.getElementById(currentid).setAttribute("src", "images/rood.png");
+    document.getElementById('line6rA').innerHTML = "<img src = 'images/rood.png'>";
 });
+
+
+
+$(".A").on("click", function (event) {
+    currentid = $(this).attr('id');
+    document.getElementById(currentid).innerHTML = "<img src = 'images/game-ball.png'>";
+});
+$(".B").on("click", function (event) {
+    currentid = $(this).attr('id');
+    document.getElementById(currentid).innerHTML = "<img src = 'images/game-ball.png'>";
+});
+$(".C").on("click", function (event) {
+    currentid = $(this).attr('id');
+    document.getElementById(currentid).innerHTML = "<img src = 'images/game-ball.png'>";
+});
+$(".D").on("click", function (event) {
+    currentid = $(this).attr('id');
+    document.getElementById(currentid).innerHTML = "<img src = 'images/game-ball.png'>";
+});
+
+
+
+$(".cb img").on("click", function (event) {
+    currentid = $(this).attr('id');
+    document.getElementById('line1lD').innerHTML = "<img src = 'images/rood.png'>";
+});
+
+
 
 
