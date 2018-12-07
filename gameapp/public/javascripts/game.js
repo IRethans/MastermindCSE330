d4b544daa3962911faa5b62ec78cf0271d2c5c14
Isa Rethans <isarethans@hotmail.com>
2018-12-07 16:03:21 +0100

    s

--- gameapp/public/javascripts/game.js
+++ gameapp/public/javascripts/game.js
@@ -1,7 +1,7 @@
 var currentline = 1;
 
 $(".cb img").on("click", function (event) {
-    var currentid = $(this).attr('id');
+    currentid = $(this).attr('id');
     document.getElementById(currentid).setAttribute("src", "images/rood.png");
 });
 
