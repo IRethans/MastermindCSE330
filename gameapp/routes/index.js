0d5eac2cc99d60677d48f279b1ede7c915089223
Isa Rethans <isarethans@hotmail.com>
2018-12-15 10:34:58 +0100

    l

--- gameapp/routes/index.js
+++ gameapp/routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/play', function(req, res) {
-  res.sendFile("game.html", {root: "./public"});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
+
+
 
 module.exports = router;
